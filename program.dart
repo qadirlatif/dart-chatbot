eb8d61095fcf0a1dc3b5f8addcc836b17d293821
Abdul Qadir Abdul Latif <92687658+qadirlatif@users.noreply.github.com>
2023-05-06 22:11:59 +0500

    Update program.dart

--- program.dart
+++ program.dart
@@ -2,73 +2,88 @@ import 'dart:io';
 
 void main() {
   File file = new File("main.txt");
-  var textTokenized = file.readAsStringSync().split('.');
-  print("******************Welcome to Dart Chatbot**********************");
-  print("Please Enter keyword Which topic do you want to learn? or write 'bye' for exit.");
+  List<String> textTokenized = file.readAsStringSync().split('.');
+  textwriter(textTokenized);
+}
+
+void textwriter(List<String> textTokenized) {
+  print("");
+  print("\t\t\tWelcome to Dart Chatbot");
+  print(
+      "_______________________________________________________________________________");
+  print("Ask me anything about Dart: ");
+  print("Please search through keywords for the topics which you want to learn or write 'bye' for exit.");
   while (true) {
-    print("You: ");
+    stdout.write("YOU-> ");
+    //print("You: ");
     String userInput = stdin.readLineSync() as String;
     userInput = userInput.toLowerCase();
+    stdout.write("BOT-> ");
     if (userInput == "bye") {
-      print("BOT: Ok Bye, Thank You!");
+      print("Good Bye, Have a great day!");
       break;
-    } else if(userInput!="") {
-      TextGenerator generator=new TextGenerator();
+    } else if (userInput != "") {
+      TextGenerator generator = new TextGenerator();
       generator.generateText(textTokenized, userInput);
-    }
-    else {
-      print("please Write a Proper Question!");
+    } else {
+      print("Please Write a Proper Question!");
     }
   }
 }
 
-class TextFinder{
-bool findingText(var spacetokenized, String text) {
-  bool check = false;
-  List<String> commonWords = [
-    "and",
-    "are",
-    "is",
-    "to",
-    "for",
-    "also",
-    "of",
-    "it's",
-    "like",
-    "a"
-  ];
-  List tokenizedText = text.split(' ');
-  spacetokenized.forEach((element) {
-    tokenizedText.forEach((element1) {
-      bool commonwordschecker = false;
-      
-      commonWords.forEach((element) {
-        if (element == element1.toLowerCase()) {
-          commonwordschecker = true;
+class TextFinder {
+  bool findingText(List<String> spacetokenized, String text) {
+    bool check = false;
+    List<String> commonWords = [
+      "and",
+      "are",
+      "is",
+      "to",
+      "for",
+      "also",
+      "of",
+      "it's",
+      "like",
+      "a"
+    ];
+    List<String> tokenizedText = text.split(' ');
+    spacetokenized.forEach((element) {
+      tokenizedText.forEach((element1) {
+        bool commonwordschecker = false;
+
+        commonWords.forEach((element) {
+          if (element == element1.toLowerCase()) {
+            commonwordschecker = true;
+          }
+        });
+        if (commonwordschecker == true) {
+        } else if (element.toLowerCase() == element1.toLowerCase()) {
+          check = true;
         }
       });
-      if (commonwordschecker == true) {
-      } else if (element.toLowerCase() == element1.toLowerCase()) {
-        check = true;
-      }
     });
-  });
-  return check;
+    return check;
+  }
 }
-}
-class TextGenerator extends TextFinder{
-void generateText(var textTokenized, String userInput) {
-  int check = 0;
-  for (int count = 0; count < textTokenized.length; count++) {
-    var spacetokenized = textTokenized[count].split(' ');
-    if (spacetokenized.length > 1 && findingText(spacetokenized, userInput)) {
-      stdout.write("-> ");
-      print("BOT: ${textTokenized[count]}");
-      ++check;
+
+class TextGenerator extends TextFinder {
+  void generateText(List<String> textTokenized, String userInput) {
+    // print(textTokenized);
+    int check = 0;
+    for (int count = 0; count < textTokenized.length; count++) {
+      List<String> spacetokenized = textTokenized[count].split(' : ');
+      List<String> colonTokenized = spacetokenized[0].split(' ');
+      // print(colonTokenized);
+      if (spacetokenized.length > 1 && findingText(colonTokenized, userInput)) {
+        //stdout.write("BOT-> ");
+        print("${textTokenized[count]}");
+        ++check;
+        break;
+      }
+    }
+    if (check == 0) {
+      //stdout.write("BOT-> ");
+      print("Sorry! I didn't Understood You");
     }
   }
-  if (check == 0) {
-    print("BOT: Sorry! I didn't Understood You");
-  }
-}
 }
